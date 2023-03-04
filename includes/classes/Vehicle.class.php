77de56432b98bc3e8f93afe807a97624342b99b5
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2023-03-04 15:22:29 +0100

    complete task 1.4

--- includes/classes/Vehicle.class.php
+++ includes/classes/Vehicle.class.php
@@ -5,5 +5,10 @@
         private $model;
         private $year;
 
+        function __construct($brand, $model, $year) {
+            $this->$brand = $brand;
+            $this->$model = $model;
+            $this->$year = $year;
+        }
     }
 ?>
